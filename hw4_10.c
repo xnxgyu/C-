b36ebc64cfa00b15962ab912cb05652a91638a3c
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-04 16:08:55 +0800

    Update hw4_10.c

--- hw4_10.c
+++ hw4_10.c
@@ -6,9 +6,9 @@ int main (void)
 int num,1 num2;
 
 printf ("輸入兩個整數以計算乘積:\n");
-scanf ("%d %d,&num1, &num2);
+scanf ("%d %d",&num1, &num2);
   
-printf ("%d x %d = %d\n"num1, num2, num1 * num2);
+printf ("%d x %d = %d\n",num1, num2, num1 * num2);
 
 system ("pause");
 return 0;
