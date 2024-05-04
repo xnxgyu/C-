a155fe8af843ddea1b44d49047848e5f938e95d3
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-04 16:06:44 +0800

    Create hw4_10.c

---
+++ hw4_10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) 
+{
+int num,1 num2;
+
+printf ("輸入兩個整數以計算乘積:\n");
+scanf ("%d %d,&num1, &num2);
+  
+printf ("%d x %d = %d\n"num1, num2, num1 * num2);
+
+system ("pause");
+return 0;
+
+}
