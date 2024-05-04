ebd8733d1c16ae11e8efeefc57e738e20f8ea9cd
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-04 17:14:53 +0800

    Create hw4_11.c

---
+++ hw4_11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) 
+{
+int stuNo, age;
+
+printf ("輸入學號");
+scanf ("%d",&stuNo);
+
+printf ("輸入年齡");
+scanf ("%d" &age);
+
+printf ("學號為: %d\t 年齡為: %d\n", stuNo, age);
+
+system("pause");
+return 0;
+
+}
