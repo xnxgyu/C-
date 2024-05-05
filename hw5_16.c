30a270338a1ebd7be0898181ca01e8c68bf3d22a
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-05 13:20:54 +0800

    Create hw5_16.c

---
+++ hw5_16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlin.h>
+
+int main(void) 
+{
+float r;
+
+printf("輸入圓球半徑:");
+scanf("%f". &r);
+
+printf("圓球體積為: %4lf\n", (double)r * r * r * 3.1415 * 4 / 3);
+
+systm("pause");
+return 0;
+
+}
