c303f809bb3e43b81fd4ebd85497837a8bcd2af9
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-09 21:39:20 +0800

    Create hw7_18a.c

---
+++ hw7_18a.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+int n, i = 2, sum = 0;
+
+do
+{
+printf("輸入一個正偶數:");
+scanf("%d",&n);
+}
+while(n % 2 != 0 || n <= 0);
+
+while(i <= n)
+{
+sum += i;
+i += 2;
+}
+printf("所有偶數到 n 的總和為 %d\n", sum);
+
+system("pause");
+return 0;
+
+}
