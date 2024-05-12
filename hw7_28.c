b8c642cbbb64a8d41414a560ccbd206d76243ab6
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-12 17:39:38 +0800

    Create hw7_28.c

---
+++ hw7_28.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main(void) 
+{
+  int i = 1, j;
+do
+  {
+  j = 1;
+  do
+{
+  printf("%d x %d = %2d  ",j, i, i * j);
+  j++;
+}
+  while(j <= 9);
+  printf("\n");
+
+  i++;
+}
+  while(i <= 9);
+
+system("pause");
+return 0;
+
+}
