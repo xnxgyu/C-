02c52cb2d8a1504a062a09b03ab5fb2f70f46afb
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-13 17:57:06 +0800

    Create hw8_1.c

---
+++ hw8_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdilb.h>
+
+void kitty(void);
+
+int main(void)
+
+{
+
+kitty();
+
+system("pause");
+return 0;
+
+}
+
+void kitty(void) 
+{
+     printf("Hello Kitty\n");
+
+}
