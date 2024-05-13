fb6c8145fa819adaeb9d250bf577084d80893de3
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-13 14:57:32 +0800

    Create hw7_32.c

---
+++ hw7_32.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+int n, i, sum = 0;
+
+do
+{
+printf("輸入一個整數: ");
+scanf("%d", &n);
+}
+while(n <= 0);
+
+for(i = 1; i <= n; i++)
+   if(n % i == 0)
+       sum += i;
+
+if(1 + n == sum)
+ printf("%d 是質數。 \n",n);
+else
+ printf("%d 不是質數。 \n", n);
+
+system("pause");
+return 0;
+
+}
