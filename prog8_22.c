4b97a619767cdfedc7dee52e254cf6c2389e2a20
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-20 20:48:16 +0800

    Create prog8_22.c

---
+++ prog8_22.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14
+double area(double);
+int main(void)
+{
+    printf("PI = %4.2f, area() = %6.2f \n",PI,area(2.0));
+    
+    system("pause");
+    return 0;
+}
+double area(double r)
+{
+    return PI * r * r;
+}
