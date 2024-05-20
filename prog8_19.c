373673fcb7fc17b7f4d4abe59883b9ee2e1eda04
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-20 18:35:44 +0800

    Create prog8_19.c

---
+++ prog8_19.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+void add10(int,int);
+int main(void)
+{
+    int a = 3, b = 5;
+    
+    printf("呼叫函數 add10() 之前:");
+    printf("a = %d,b = %d \n",a, b);
+    add10(a,b);
+    printf("呼叫函數 add10() 之後:");
+    printf("a = %d,b = %d \n",a, b);
+    
+    system("pause");
+    return 0;
+}
+void add10(int a, int b)
+{
+    a = a + 10;
+    b = b + 10;
+    printf("a = %d,b = %d \n",a, b); //理解用測試
+}
