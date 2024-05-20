91f9549fba077eff73ff16cbf6f1e438029ffce8
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-20 20:41:07 +0800

    Create prog8_21.c

---
+++ prog8_21.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define WORD "Think of all the things \
+we've shared and seen. \n"
+int main(void)
+{
+    printf(WORD);
+     
+    system("pause");
+    return 0;
+}
