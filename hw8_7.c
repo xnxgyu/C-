74ed605d56475c39e13d77fc951f445a28a61a49
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-22 20:37:46 +0800

    Create hw8_7.c

---
+++ hw8_7.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int prime(int);
+
+int main(void)
+{
+    int n = 169;
+    
+    printf("第 %d 個質數為 %d \n", n, prime(n));
+    
+    system("pause");
+    return 0;
+}
+
+
+int prime(int n)
+{
+    int i, j, c = 1;
+    
+    for(i = 2; i > 0; i++)
+    {
+        for(j = 2; j < i; j++)
+        {
+            if(i % j == 0)
+                break;
+            if(j == i - 1)
+                c += 1;
+        }  
+        
+        if(c == n)
+            return i;
+    }
+    
+    return -1;
+}
