4a11c4181b48d80e9d6ebe9de0b87c710e5569b8
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-24 19:53:12 +0800

    Create hw8_16.c

---
+++ hw8_16.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int find_k(int);
+
+int main(void)
+{
+    int n;
+    
+    printf("輸入一個整數 n 將從 4k + 2 找到一個最接近 n 的 k 值:");
+    scanf("%d",&n);
+    printf("k值為 %d \n", find_k(n));
+    
+    system("pause");
+    return 0;
+           
+}
+           
+int find_k(int n)
+{
+    int k = 1, out = 2;
+    
+    while(1)
+    {
+        if(abs(out - n) <= abs((4 * k + 2) - n))
+            return k - 1;
+        else
+        {
+            out = 4 * k + 2;
+            k++;
+        }
+        
+    }
+    
+}
+           
+           
+           
