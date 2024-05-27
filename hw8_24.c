117756b5db53e090f5f663dea0da4f2cf3a7d103
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-27 16:12:05 +0800

    Create hw8_24.c

---
+++ hw8_24.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void counter(void);
+
+int main(void)
+{
+    counter();
+    counter();
+    
+    system("pause");
+    return 0;
+    
+}
+
+void counter(void)
+{
+    static int cont = 0;
+    
+    printf("counter() 已經被呼叫 %d 次了...\n",++cont);
+    
+}
