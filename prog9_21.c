36da908dea0911985bcd462c1be428c5797e9b2f
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-05-31 14:54:43 +0800

    Create prog9_21.c

---
+++ prog9_21.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    char name[15];
+    
+    puts("What's your name?");
+    gets(name);
+    puts("Hi!");
+    puts(name);
+    puts("How are you?");
+    
+system("pause");
+return 0;
+    
+}
