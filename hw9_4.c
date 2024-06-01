d1040c5b01f8122881132f382a7c33f3394bfc6e
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-06-01 11:44:34 +0800

    Create hw9_4.c

---
+++ hw9_4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 3
+
+int main(void)
+{
+    int i;
+    int arr[SIZE];
+    
+    for(i = 0;i < SIZE; i++)
+    {
+        printf("輸入第 %d 個整數: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    
+    for(i = 0; i < SIZE; i++)
+        printf("arr[%d] = %d \n", i, arr[i]);
+    
+    system("pause");
+    return 0;
+}
