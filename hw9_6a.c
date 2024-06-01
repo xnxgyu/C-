7e09a74b2359272fd54f05f6680f4138eee6d6d7
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-06-01 11:56:51 +0800

    Create hw9_6a.c

---
+++ hw9_6a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int array[] = {3, 5, 0, 3, 2, 4, 1, 6, 8, 5, 4, 3, 2};
+    
+    printf("number of elements of array[] = %d \n", sizeof(array) / sizeof(int));
+    
+    system("pause");
+    return 0;
+    
+}
