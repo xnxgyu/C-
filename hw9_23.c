16042fef6352f1432197ae2976247d855ea8df55
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-06-05 13:59:05 +0800

    Create hw9_23.c

---
+++ hw9_23.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char str[] = "Hello C language";
+    
+    printf("ste[] = %s\n",str);
+    printf("sizeof(str[]) = %d\n",sizeof(str));
+    
+    system("pause");
+    return 0;
+    
+}
