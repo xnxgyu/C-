8a27b41baa47e0f2372cc9ffe76792b9d40520ba
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-06-06 14:51:53 +0800

    Create prog10_4.c

---
+++ prog10_4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int *ptri;
+    char *ptrc;
+    
+    printf("sizeof(ptri)=%d\n",sizeof(ptri));
+    printf("sizeof(ptrc)=%d\n",sizeof(ptrc));
+    printf("sizeof(*ptri)=%d\n",sizeof(*ptri));
+    printf("sizeof(*ptrc)=%d\n",sizeof(*ptrc));
+    
+    system("Pause");
+    return 0;
+    
+}
+
+    
