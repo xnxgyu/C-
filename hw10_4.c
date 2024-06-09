405d66aff80fe1577dc86e6809432d6edf9975e8
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-06-09 13:38:22 +0800

    Create hw10_4.c

---
+++ hw10_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    float num = 12.6f, *ptr;
+    ptr = &num;
+    
+    printf("num=12.6,\n&num = %p\nptr=%p\n",&num,&ptr);
+    
+    system("pause");
+    return 0;
+}
