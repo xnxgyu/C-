c728e0fc28c447680fbd0f7af2feaed2045db4f4
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-06-09 13:36:06 +0800

    Create hw10_3.c

---
+++ hw10_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    float b1 = 3.14;
+    int num = 5;
+    
+    printf("b1 = 3.14, address of b1 is %p\n",&b1);
+    printf("num = 5, address of num is %p\n",&num);
+    
+    system("pause");
+    return 0;
+}
