eedd84c54d876227868194329c9c9efff356d198
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-06-09 21:27:04 +0800

    Create 重點是括號 hw10_13.c

---
+++ 重點是括號 hw10_13.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num[] = {14, 23, 32, 62, 19};
+    int *p1, *p2;
+    
+    p1 = p2 = num;
+    
+    *p1++;
+    printf("*p1=%d\n",*p1);
+    
+    (*p2)++;
+    printf("*p2 = %d\n",*p2);
+    
+    return 0;
+  
+}
