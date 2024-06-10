0d4df6f59614268349204891920626c2776fbbfa
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-06-10 18:32:43 +0800

    Create hw10_23.c

---
+++ hw10_23.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ROW 2
+#define COL 3
+int main(void)
+{
+    int i, j;
+    int A[ROW][COL] = {{1, 2, 3}, {5, 6, 7}};
+    int B[ROW][COL] = {{3, 0, 2}, {3, 5, 7}};
+    
+    printf("Matrix A + B =\n");
+    
+    for(i=0;i<ROW;i++)
+    {
+        for(j=0;j<COL;j++)
+            printf("%3d", *(*(A + i) + j) + *(*(B + i) + j));
+        printf("\n");
+        
+    }
+    return 0;
+    
+        
+}
