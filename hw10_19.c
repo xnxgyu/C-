5c0cfe3d422c84ee0ab30397c9174a9937e3834a
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-06-10 16:56:45 +0800

    Create hw10_19.c

---
+++ hw10_19.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 20
+
+int length(char *ptr);
+
+int main(void)
+{
+    char usersay[SIZE];
+    
+    printf("Write somrething here: ");
+    gets(usersay);
+    
+    printf("You entered %d characters here.\n", length(usersay));
+    
+    return 0;
+}
+
+int length(char *ptr)
+{
+    int i;
+    
+    for(i-0; *(ptr + i) != '\0'; i++);
+        
+        return i;
+}
