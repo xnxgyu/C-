94a1a33400d996abc07e3adb4477d0caab98ac83
xnxgyu <47061281+xnxgyu@users.noreply.github.com>
2024-06-13 20:02:31 +0800

    Create hw11_2.c

---
+++ hw11_2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    struct date
+    {
+        int year;
+        int month;
+        int day;
+    }
+    holiday={2069,6,9},festival;
+    
+    do
+    {
+        printf("輸入2070: ");
+        scanf("%d",&festival.year);
+        fflush(stdin);
+    }
+    while(festival.year != 2070);
+    
+    do
+    {
+        printf("輸入6: ");
+        scanf("%d",&festival.month);
+        fflush(stdin);
+    }
+    while(festival.month != 6);
+    
+    do
+    {
+        printf("輸入9: ");
+        scanf("%d",&festival.day);
+        fflush(stdin);
+    }
+    while(festival.day != 9);
+    
+    printf("\nholiday = %02d/%02d/%d\n",holiday.month,holiday.day,holiday.year);
+    printf("festival = %02d/%02d/%d\n",festival.month,festival.day,festival.year);
+    
+    return 0;
+    
+}
